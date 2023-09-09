3aee6912e8f791ef2ee5c2ca388f9c217a4f1545
developbyarya <aryain3rd@gmail.com>
2023-09-09 22:27:02 +0700

    Perbaikan: Ukuran di layar, pergerakan dan jarak

--- detak jantung.js
+++ detak jantung.js
@@ -1,10 +1,13 @@
-const Detak_Jantung=document.getElementById ("Detak Jantung")
+const Detak_Jantung = document.getElementById("detak");
 
-fetch("https://demo-tiger.vercel.app/bpm").then((Tiger)=> {
-    return Tiger.json()
-}).then((Tiger) => 
-{ let i = 0;
-    setInterval(() => 
-    { Detak_Jantung.innerText = Tiger[i];
-    i++ }, 60000);
-});
+fetch("https://demo-tiger.vercel.app/bpm")
+  .then((Tiger) => {
+    return Tiger.json();
+  })
+  .then((Tiger) => {
+    let i = 0;
+    setInterval(() => {
+      Detak_Jantung.innerText = Tiger[i];
+      i++;
+    }, 1000);
+  });
--- kadar oksigen.js
+++ kadar oksigen.js
@@ -1,10 +1,13 @@
-const Saturasi=document.getElementById ("Saturasi")
+const Saturasi = document.getElementById("Saturasi");
 
-fetch("https://demo-tiger.vercel.app/oksigen").then((Tiger)=> {
-    return Tiger.json()
-}).then((Tiger) => 
-{ let i = 0;
-    setInterval(() => 
-    { Saturasi.innerText = Tiger[i];
-    i++ }, 60000);
-});
+fetch("https://demo-tiger.vercel.app/oksigen")
+  .then((Tiger) => {
+    return Tiger.json();
+  })
+  .then((Tiger) => {
+    let i = 0;
+    setInterval(() => {
+      Saturasi.innerText = Tiger[i];
+      i++;
+    }, 1000);
+  });
